a24b983d6b7903903d018a13553936351bfbf05c
atrautsch <alexander.trautsch@cs.uni-goettingen.de>
2018-06-01 11:07:10 +0200

    override css after dependency update

--- frontend/app/src/components/Header.vue
+++ frontend/app/src/components/Header.vue
@@ -91,7 +91,9 @@ export default {
 }
 
 header.navbar .navbar-brand {
-  background-image: none;
+  background-image: none !important;
+  font-size: 1rem;
+  padding-top: 0.9rem !important;
 }
 
 .navbar-brand {
