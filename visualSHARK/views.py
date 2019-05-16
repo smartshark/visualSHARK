5880c59a7f7bbbbd47df576f3880ec992b55849e
atrautsch <alexander.trautsch@cs.uni-goettingen.de>
2019-05-16 15:21:00 +0200

    include szz

--- visualSHARK/views.py
+++ visualSHARK/views.py
@@ -974,7 +974,7 @@ class IssueLinkSet(APIView):
         result = {}
         result['commits'] = []
         limit = int(request.GET["limit"])
-        commits = Commit.objects.filter(Q(vcs_system_id=request.GET["vcs_system_id"])).filter(Q(validations__ne='issue_links')).filter(Q(labels__issueonly_bugfix=True) or Q(labels__adjustedszz_bugfix=True)).only('id', 'message', 'linked_issue_ids', 'labels', 'szz_issue_ids').order_by('?')[:limit]
+        commits = Commit.objects.filter(Q(vcs_system_id=request.GET["vcs_system_id"])).filter(Q(validations__ne='issue_links')).filter(Q(labels__issueonly_bugfix=True) | Q(labels__adjustedszz_bugfix=True)).only('id', 'message', 'linked_issue_ids', 'labels', 'szz_issue_ids').order_by('?')[:limit]
         for commit in commits:
             if len(commit.linked_issue_ids) > 0:
                 result_commit = {}
