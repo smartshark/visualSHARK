7e28d2fb30a6c503ab451ad232bc8f3bb784a92f
atx <atx@drecks-provider.de>
2020-05-15 20:17:05 +0200

    add lable info to help

--- frontend/app/src/views/Help.vue
+++ frontend/app/src/views/Help.vue
@@ -13,7 +13,18 @@
 
           <strong>Labeling Information</strong>
           <p>
-            The commits/lines that should be labeled are randomly drawn from all available and manually validated issues for the project that is selected on the right side.
+            The commits/lines that should be labeled are randomly drawn from all available, manually validated issues for the project that is selected on the right side.
+          </p>
+          <p>
+          <strong>Description of labels</strong><br/>
+          <ol>
+            <li>Bugfix: The line contributes to the corrective change that is performed to address the issue that is described.</li>
+            <li>Whitespace: The line only contains changes to whitespaces that do not affect the logic of the source code.</li>
+            <li>Documentation: The line only contains changes to documentation of the software, including line comments or documentation files.</li>
+            <li>Refactoring: The change is a refactoring, e.g., a renaming of a variable or the extraction of a method.</li>
+            <li>Test: The change is only to tests of the project, e.g., test code or test data.</li>
+            <li>Unrelated: The change is neither of the above, e.g., the addition of features unrelated to the described issue.</li>
+          </ol>
           </p>
         </div>
       </div>
